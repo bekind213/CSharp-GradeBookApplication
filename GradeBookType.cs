f6333a3c5719ff2fcbe3bb5bd5e7eebfc8dd6b8e
bekind213 <40498295+bekind213@users.noreply.github.com>
2018-06-22 14:44:21 -0500

    Add files via upload

---
+++ GradeBookType.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType  //INSTRUCTIONS: This should be located in the Enums directory. This should use the public access modifier.  This should use the GradeBooks.Enums namespace.
+    {   //This should contain the values Standard, Ranked, ESNU, OneToFour, and SixPoint.
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
